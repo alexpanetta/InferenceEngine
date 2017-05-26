484d014803a0a68ba33ff3f4fb1e516f905f9554
Alex <100583190@student.swin.edu.au>
2017-05-26 14:25:09 +1000

    Update Method

--- src/iengine/Method.java
+++ src/iengine/Method.java
@@ -9,23 +9,24 @@ public abstract class Method {
     public String code; //code used to identify the method at the command line
     public String name; //actual name of the method
 
-    //variables for chaining methods
+    //instances for chaining methods and truth tabling
     public HashMap<String, Integer> Count;
     public HashMap<String, Boolean> Inferred;
     public LinkedList<String> Agenda;
     public HashSet<HornClause> KnowledgeBase;   //testing for TT
     public Integer ValidModels;
+    public LinkedList<String> Entailed;
     public StringBuilder Solution;
 
-    public abstract String Solve(String kb, String query);
-    
-    //Initialising KB
-    //public abstract void InitKB(String kb);        
+    public abstract String Solve(String kb, String query);     
     
     public void reset() {
         this.Agenda.clear();
         this.Inferred.clear();
         this.Count.clear();
+        this.KnowledgeBase.clear();
+        this.ValidModels = 0;
+        this.Entailed.clear();
         this.Solution.setLength(0);
     }
 }
